5327888c2c3003e50a06fc2e0ccca47ff4ddb52e
nicross <nicross@users.noreply.github.com>
2020-07-10 15:14:28 -0500

    Define basic SFX

---
+++ src/js/content/sfx.js
@@ -0,0 +1,12 @@
+content.sfx = {}
+
+content.sfx.bus = engine.audio.mixer.createBus()
+content.sfx.bus.gain.value = engine.utility.fromDb(-6)
+
+content.sfx.gameOver = () => {
+  // TODO
+}
+
+content.sfx.lap = () => {
+  // TODO
+}
