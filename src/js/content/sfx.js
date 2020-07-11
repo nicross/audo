1f11ed31a23c57818728dedd0fd2fab91d0dda5a
nicross <nicross@users.noreply.github.com>
2020-07-11 18:31:54 -0500

    Add game over sfx

--- src/js/content/sfx.js
+++ src/js/content/sfx.js
@@ -1,10 +1,52 @@
 content.sfx = {}
 
 content.sfx.bus = engine.audio.mixer.createBus()
-content.sfx.bus.gain.value = engine.utility.fromDb(-6)
+content.sfx.bus.gain.value = engine.utility.fromDb(0)
 
 content.sfx.gameOver = () => {
-  // TODO
+  const now = engine.audio.time()
+
+  const noise = engine.audio.synth.createBuffer({
+    buffer: engine.audio.buffer.noise.pink(),
+  }).filtered({
+    frequency: 120,
+  }).connect(content.sfx.bus)
+
+  noise.param.gain.setValueAtTime(engine.const.zeroGain, now)
+  noise.param.gain.exponentialRampToValueAtTime(8, now + 1/16) // XXX: Above 0 dB
+  noise.param.gain.exponentialRampToValueAtTime(engine.const.zeroGain, now + 4)
+
+  noise.stop(now + 4)
+
+  const tone = engine.audio.synth.createSimple({
+    frequency: engine.utility.midiToFrequency(90),
+  }).connect(content.sfx.bus)
+
+  tone.param.gain.setValueAtTime(engine.const.zeroGain, now)
+  tone.param.gain.linearRampToValueAtTime(1/256, now + 1.5)
+  tone.param.gain.linearRampToValueAtTime(engine.const.zeroGain, now + 3)
+
+  tone.stop(now + 3)
+
+  const zap = engine.audio.synth.createBuffer({
+    buffer: engine.audio.buffer.noise.white(),
+  }).filtered({
+    type: 'bandpass',
+  }).connect(content.sfx.bus)
+
+  zap.param.gain.setValueAtTime(engine.const.zeroGain, now)
+  zap.param.gain.exponentialRampToValueAtTime(1/32, now + 1/32)
+  zap.param.gain.linearRampToValueAtTime(engine.const.zeroGain, now + 2)
+
+  zap.filter.frequency.setValueAtTime(engine.const.minFrequency, now)
+  zap.filter.frequency.exponentialRampToValueAtTime(engine.const.maxFrequency, now + 1/4)
+  zap.filter.frequency.exponentialRampToValueAtTime(engine.const.minFrequency, now + 2)
+
+  zap.filter.Q.setValueAtTime(1, now)
+  zap.filter.Q.linearRampToValueAtTime(10, now + 1/32)
+  zap.filter.Q.linearRampToValueAtTime(1, now + 2)
+
+  zap.stop(now + 2)
 }
 
 content.sfx.lap = () => {
@@ -19,7 +61,7 @@ content.sfx.lap = () => {
   synth.param.detune.linearRampToValueAtTime(1200, now + 1/4)
 
   synth.param.gain.setValueAtTime(engine.const.zeroGain, now)
-  synth.param.gain.exponentialRampToValueAtTime(0.25, now + 1/32)
+  synth.param.gain.exponentialRampToValueAtTime(1/32, now + 1/32)
   synth.param.gain.exponentialRampToValueAtTime(engine.const.zeroGain, now + 3)
 
   synth.stop(now + 3)
