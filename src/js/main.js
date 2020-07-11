ed4a87798af716d9e020ddb862760b32c774fe69
nicross <nicross@users.noreply.github.com>
2020-07-11 18:40:06 -0500

    Decrease reverb

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   engine.audio.mixer.master.param.limiter.ratio.value = 10
   engine.audio.mixer.master.param.limiter.release.value = 0.25
   engine.audio.mixer.master.param.limiter.threshold.value = -24
+
+  engine.audio.mixer.auxiliary.reverb.setImpulse(engine.audio.buffer.impulse.medium())
+  engine.audio.mixer.auxiliary.reverb.setGain(engine.utility.fromDb(-3))
 })
