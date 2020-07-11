efee89060f5c44d9dfe8c42c2688f68d4725bf1c
nicross <nicross@users.noreply.github.com>
2020-07-11 18:11:53 -0500

    Add lap sfx

--- src/js/content/sfx.js
+++ src/js/content/sfx.js
@@ -8,5 +8,19 @@ content.sfx.gameOver = () => {
 }
 
 content.sfx.lap = () => {
-  // TODO
+  const now = engine.audio.time()
+
+  const synth = engine.audio.synth.createSimple({
+    frequency: engine.utility.midiToFrequency(60),
+  }).connect(content.sfx.bus)
+
+  synth.param.detune.setValueAtTime(1200, now)
+  synth.param.detune.linearRampToValueAtTime(engine.const.zero, now + 1/16)
+  synth.param.detune.linearRampToValueAtTime(1200, now + 1/4)
+
+  synth.param.gain.setValueAtTime(engine.const.zeroGain, now)
+  synth.param.gain.exponentialRampToValueAtTime(0.25, now + 1/32)
+  synth.param.gain.exponentialRampToValueAtTime(engine.const.zeroGain, now + 3)
+
+  synth.stop(now + 3)
 }
