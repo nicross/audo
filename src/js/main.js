fd08da2e6b36773c03d7281ea72ef354cb15490b
nicross <nicross@users.noreply.github.com>
2020-07-12 10:27:53 -0500

    Add boost with limiter

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
   app.activate()
   engine.loop.start().pause()
 
-  engine.audio.mixer.master.param.limiter.attack.value = 0.009
+  engine.audio.mixer.master.param.limiter.attack.value = 0.003
   engine.audio.mixer.master.param.limiter.gain.value = 1
-  engine.audio.mixer.master.param.limiter.knee.value = 12
-  engine.audio.mixer.master.param.limiter.ratio.value = 10
-  engine.audio.mixer.master.param.limiter.release.value = 0.25
-  engine.audio.mixer.master.param.limiter.threshold.value = -24
+  engine.audio.mixer.master.param.limiter.knee.value = 15
+  engine.audio.mixer.master.param.limiter.ratio.value = 15
+  engine.audio.mixer.master.param.limiter.release.value = 0.125
+  engine.audio.mixer.master.param.limiter.threshold.value = -30
 
   engine.audio.mixer.auxiliary.reverb.setImpulse(engine.audio.buffer.impulse.medium())
   engine.audio.mixer.auxiliary.reverb.setGain(engine.utility.fromDb(-3))
