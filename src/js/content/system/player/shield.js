776686748a02de58f4501af65060db5ff2970d20
nicross <nicross@users.noreply.github.com>
2020-07-11 23:16:18 -0500

    Add pubsub to player shield

--- src/js/content/system/player/shield.js
+++ src/js/content/system/player/shield.js
@@ -1,17 +1,21 @@
 content.system.player.shield = (() => {
+  const pubsub = engine.utility.pubsub.create()
+
   let active = false
 
-  return {
+  return engine.utility.pubsub.decorate({
     grant: function () {
       active = true
+      pubsub.emit('grant')
       content.sfx.shieldUp()
       return this
     },
     has: () => active,
     remove: function () {
       active = false
+      pubsub.emit('remove')
       content.sfx.shieldDown()
       return this
     },
-  }
+  }, pubsub)
 })()
