37010e62b6c37e036f900721501c82a4f39662aa
nicross <nicross@users.noreply.github.com>
2020-07-11 20:13:38 -0500

    Add basic powerup sound

--- src/js/content/prop/powerup/base.js
+++ src/js/content/prop/powerup/base.js
@@ -1,7 +1,28 @@
 content.prop.powerup.base = engine.prop.base.invent({
   name: 'powerup/base',
+  onConstruct: function () {
+    const frequency = engine.utility.midiToFrequency(72)
+    const depth = frequency / 2
+
+    this.synth = engine.audio.synth.createMod({
+      amodDepth: 0.5,
+      amodFrequency: 16,
+      amodType: 'square',
+      carrierGain: 0.5,
+      carrierFrequency: frequency + (depth / 2),
+      carrierType: 'triangle',
+      fmodDepth: depth,
+      fmodType: 'sawtooth',
+      fmodFrequency: 4,
+    }).connect(this.output.input)
+
+    this.synth.param.gain.value = engine.utility.fromDb(-3)
+  },
+  onDestroy: function () {
+    this.synth.stop()
+  },
   applyBoost: function () {
-    const velocity = content.system.player.getVelocity()
+    const velocity = content.system.player.velocity()
     content.system.player.addVelocity(velocity * 0.5)
     content.sfx.boost()
     return this
@@ -19,4 +40,13 @@ content.prop.powerup.base = engine.prop.base.invent({
     content.system.player.shield.grant()
     return this
   },
+  setCooldown: function (state) {
+    this.isCooldown = Boolean(state)
+
+    if (state) {
+      engine.audio.ramp.exponential(this.synth.param.gain, engine.const.zeroGain, 1)
+    } else {
+      engine.audio.ramp.linear(this.synth.param.gain, engine.utility.fromDb(-3), 1)
+    }
+  }
 })
