3288daae517ab430fdc9440002816b2b557966d4
nicross <nicross@users.noreply.github.com>
2020-07-12 12:54:51 -0500

    Fix basic app audio crashes

---
+++ src/js/app/crashFixer.js
@@ -0,0 +1,23 @@
+app.crashFixer = (() => {
+  const context = engine.audio.context()
+
+  const analyzer = context.createAnalyser()
+  analyzer.fftSize = 32
+  engine.audio.mixer.master.output.connect(analyzer)
+
+  const analyzerTimeData = new Uint8Array(analyzer.frequencyBinCount)
+
+  return {
+    update: function () {
+      analyzer.getByteTimeDomainData(analyzerTimeData)
+
+      if (isNaN(analyzerTimeData[0]) || !isFinite(analyzerTimeData[0])) {
+        engine.audio.mixer.rebuildFilters()
+      }
+
+      return this
+    },
+  }
+})()
+
+engine.loop.on('frame', () => app.crashFixer.update())
