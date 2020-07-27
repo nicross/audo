9afa3034f2afee9b0c670d3595b75da04da7e847
nicross <nicross@users.noreply.github.com>
2020-07-27 14:54:34 -0500

    Disable devTools in Electron builds 😱

--- electron/main.js
+++ electron/main.js
@@ -22,7 +22,7 @@ function createWindow() {
     icon: path.join(__dirname, '../public/favicon.png'),
     title: package.name,
     webPreferences: {
-      devTools: true, // TODO: Disable in submitted build
+      devTools: false,
       preload: path.join(__dirname, 'preload.js'),
     }
   })
