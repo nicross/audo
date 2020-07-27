cd60b1ddc25fb0ed47a1d84773b5a78b14ae5336
nicross <nicross@users.noreply.github.com>
2020-07-27 18:18:27 -0500

    Convert wind to stereo

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -3,6 +3,7 @@
 ## v1.1.0
 - Added sound effect to race start
 - Applied invincibility for duration of shield granted sound effect
+- Converted wind to stereo
 - Fixed garbage collection issue with dynamic soundtrack
 - Improved audibility of sound effects
 - Made collision sounds more impactful
--- src/js/content/system/player/audio/wind.js
+++ src/js/content/system/player/audio/wind.js
@@ -1,22 +1,35 @@
 content.system.player.audio.wind = (() => {
-  const synth = engine.audio.synth.createBuffer({
+  const context = engine.audio.context(),
+    filter = context.createBiquadFilter(),
+    merger = context.createChannelMerger(2),
+    mix = context.createGain()
+
+  engine.audio.synth.createBuffer({
     buffer: engine.audio.buffer.noise.brown(),
-  }).filtered({
-    frequency: engine.const.maxFrequency,
-  }).connect(content.system.player.audio.bus())
+    gain: 0.5,
+  }).connect(merger, 0, 0)
+
+  engine.audio.synth.createBuffer({
+    buffer: engine.audio.buffer.noise.brown(),
+    gain: 0.5,
+  }).connect(merger, 0, 1)
+
+  merger.connect(filter)
+  filter.connect(mix)
+  mix.connect(content.system.player.audio.bus())
 
   return {
     reset: function () {
-      synth.filter.frequency.value = engine.const.maxFrequency
-      synth.param.gain.value = engine.const.zeroGain
+      filter.frequency.value = engine.const.maxFrequency
+      mix.gain.value = engine.const.zeroGain
 
       return this
     },
     update: function () {
       const strength = content.system.player.velocityRatio()
 
-      synth.filter.frequency.value = engine.utility.lerpExp(engine.const.minFrequency, engine.const.maxFrequency, strength, 2)
-      synth.param.gain.value = engine.utility.fromDb(engine.utility.lerp(-36, -18, strength))
+      filter.frequency.value = engine.utility.lerpExp(engine.const.minFrequency, engine.const.maxFrequency, strength, 2)
+      mix.gain.value = engine.utility.fromDb(engine.utility.lerp(-30, -18, strength))
 
       return this
     },
