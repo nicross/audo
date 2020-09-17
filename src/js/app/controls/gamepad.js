fd3da29a3ba1bee3c7981920f1f5dac414f15c29
nicross <nicross@users.noreply.github.com>
2020-09-17 13:09:30 -0500

    Leverage engine.input within app.controns

--- src/js/app/controls/gamepad.js
+++ src/js/app/controls/gamepad.js
@@ -1,35 +1,11 @@
 app.controls.gamepad = {
-  deadzone: (input, threshold = 0.1875) => {
-    const ratio = (Math.abs(input) - threshold) / (1 - threshold),
-      sign = input > 0 ? 1 : -1
-
-    return ratio > 0 ? sign * ratio : 0
-  },
   game: function () {
-    const gamepads = navigator.getGamepads()
-
-    if (!gamepads.length) {
-      return {}
-    }
-
-    const buttons = {}
+    const {digital: buttons} = engine.input.gamepad.get()
 
     let x = 0
 
-    for (let i = 0; i < gamepads.length; i += 1) {
-      const gamepad = gamepads[i]
-
-      if (!gamepad) {
-        continue
-      }
-
-      if (0 in gamepad.axes && 1 in gamepad.axes) {
-        x += this.deadzone(gamepad.axes[0])
-      }
-
-      gamepad.buttons.forEach((button, i) => {
-        buttons[i] |= button.pressed
-      })
+    if (engine.input.gamepad.hasAxis(0)) {
+      x += engine.input.gamepad.getAxis(0)
     }
 
     const left = buttons[2] || buttons[4] || buttons[14],
@@ -46,30 +22,12 @@ app.controls.gamepad = {
     }
 
     return {
-      x,
+      x: engine.utility.clamp(x, -1, 1),
     }
   },
   ui: function () {
-    const gamepads = navigator.getGamepads()
-
-    if (!gamepads.length) {
-      return {}
-    }
-
-    const buttons = {},
-      state = {}
-
-    for (let i = 0; i < gamepads.length; i += 1) {
-      const gamepad = gamepads[i]
-
-      if (!gamepad) {
-        continue
-      }
-
-      gamepad.buttons.forEach((button, i) => {
-        buttons[i] |= button.pressed
-      })
-    }
+    const {digital: buttons} = engine.input.gamepad.get()
+    const state = {}
 
     if (buttons[0] || buttons[8] || buttons[9]) {
       state.confirm = true
--- src/js/app/controls/keyboard.js
+++ src/js/app/controls/keyboard.js
@@ -1,71 +1,38 @@
-app.controls.keyboard = (() => {
-  const controls = {
-    ArrowLeft: false,
-    ArrowRight: false,
-    Enter: false,
-    KeyA: false,
-    KeyD: false,
-    Numpad4: false,
-    Numpad6: false,
-    NumpadEnter: false,
-    Space: false,
-  }
+app.controls.keyboard = {
+  game: () => {
+    const keys = engine.input.keyboard.get()
 
-  window.addEventListener('keydown', (e) => {
-    if (e.repeat) {
-      return
+    const left = keys.ArrowLeft || keys.KeyA || keys.Numpad4,
+      right = keys.ArrowRight || keys.KeyD || keys.Numpad6
+
+    let x = 0
+
+    if (left && !right) {
+      x = -1
+    } else if (right && !left) {
+      x = 1
     }
 
-    if (e.code in controls) {
-      controls[e.code] = true
+    if (!x) {
+      return {}
     }
-  })
 
-  window.addEventListener('keyup', (e) => {
-    if (e.code in controls) {
-      controls[e.code] = false
+    return {
+      x,
     }
-  })
+  },
+  ui: () => {
+    const keys = engine.input.keyboard.get(),
+      state = {}
 
-  return {
-    game: () => {
-      const left = controls.ArrowLeft || controls.KeyA || controls.Numpad4,
-        right = controls.ArrowRight || controls.KeyD || controls.Numpad6
+    if (keys.Enter || keys.NumpadEnter) {
+      state.enter = true
+    }
 
-      let x = 0
+    if (keys.Space) {
+      state.space = true
+    }
 
-      if (left && !right) {
-        x = -1
-      } else if (right && !left) {
-        x = 1
-      }
-
-      if (!x) {
-        return {}
-      }
-
-      return {
-        x,
-      }
-    },
-    reset: function () {
-      Object.keys(controls)
-        .forEach((key) => controls[key] = false)
-
-      return this
-    },
-    ui: () => {
-      const state = {}
-
-      if (controls.Enter || controls.NumpadEnter) {
-        state.enter = true
-      }
-
-      if (controls.Space) {
-        state.space = true
-      }
-
-      return state
-    },
-  }
-})()
+    return state
+  },
+}
